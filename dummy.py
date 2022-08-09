e5668c54efe0c187b82ed1aee3427e671d13834c
akrobi <gwangui00000@gmail.com>
2022-08-09 20:32:42 +0300

    tests functions in isolation to see how they work

---
+++ dummy.py
@@ -0,0 +1,19 @@
+import json
+import pandas as pd
+from textblob import TextBlob
+
+
+def read_json(json_file: str)->list:
+    '''json file reader to open and read json files into a list'''
+    tweets_data = []
+    count = 0 #limits the output to 100 tweets for now
+    for tweets in open(json_file, 'r'):
+        count += 1
+        tweets_data.append(json.loads(tweets))
+        if count == 5:
+            break
+
+    return len(tweets_data), tweets_data
+
+globaldata = read_json("data/global_twitter_data/global_twitter_data.json")
+print(globaldata)
