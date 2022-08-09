7692c916dc39c952a79a3638e8295b302d7b4bb7
akrobi <gwangui00000@gmail.com>
2022-08-09 20:43:48 +0300

    adds the data from global_twitter_data.json into a dataframe on pandas

--- dummy.py
+++ dummy.py
@@ -6,14 +6,16 @@ from textblob import TextBlob
 def read_json(json_file: str)->list:
     '''json file reader to open and read json files into a list'''
     tweets_data = []
-    count = 0 #limits the output to 100 tweets for now
+    # count = 0 #gets the first 5 tweets for now
     for tweets in open(json_file, 'r'):
-        count += 1
+        # count += 1
         tweets_data.append(json.loads(tweets))
-        if count == 5:
-            break
+        # if count == 5:
+        #     break
+
+    df = pd.read_json("data/global_twitter_data/global_twitter_data.json")
 
     return len(tweets_data), tweets_data
 
-globaldata = read_json("data/global_twitter_data/global_twitter_data.json")
-print(globaldata)
+# globaldata = read_json("data/global_twitter_data/global_twitter_data.json")
+# print(globaldata)
